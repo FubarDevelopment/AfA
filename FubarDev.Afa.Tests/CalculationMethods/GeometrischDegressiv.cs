2bd9e5fbc743ce81c7a86c1d4360719ae85d8319
Mark Junker <m.junker@fubar-dev.de>
2014-10-15 19:47:52 +0200

    Tests hinzugefügt

---
+++ FubarDev.Afa.Tests/CalculationMethods/GeometrischDegressiv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FubarDev.Afa.Tests.CalculationMethods
+{
+    [TestClass]
+    public class GeometrischDegressiv
+    {
+        private void TestResult(CalculationMethodResult result, decimal expectedRemainingValue, decimal expectedDepreciation)
+        {
+            Assert.AreEqual(expectedRemainingValue, result.RemainingValue);
+            Assert.AreEqual(expectedDepreciation, result.Depreciation);
+        }
+
+        [TestMethod]
+        public void TestGeometrischDegressiv1()
+        {
+            var cfg = new Afa.CalculationConfiguration();
+            var data = new Afa.CalculationData(150000, 18000, 5);
+            var rounding = new Afa.CalculationRoundings.FullValueRounding();
+            var calc = new Afa.CalculationMethods.GeometrischDegressiv();
+
+            TestResult(rounding.Calculate(calc, data, 0), 150000, 0);
+            TestResult(rounding.Calculate(calc, data, 1), 98160, 51840);
+            TestResult(rounding.Calculate(calc, data, 2), 64236, 33924);
+            TestResult(rounding.Calculate(calc, data, 3), 42036, 22200);
+            TestResult(rounding.Calculate(calc, data, 4), 27508, 14528);
+            TestResult(rounding.Calculate(calc, data, 5), 18000, 9508);
+        }
+    }
+}
---
+++ FubarDev.Afa.Tests/CalculationMethods/Linear.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FubarDev.Afa.Tests.CalculationMethods
+{
+    [TestClass]
+    public class Linear
+    {
+        private void TestResult(CalculationMethodResult result, decimal expectedRemainingValue, decimal expectedDepreciation)
+        {
+            Assert.AreEqual(expectedRemainingValue, result.RemainingValue);
+            Assert.AreEqual(expectedDepreciation, result.Depreciation);
+        }
+
+        [TestMethod]
+        public void TestLinear1()
+        {
+            var cfg = new Afa.CalculationConfiguration();
+            var data = new Afa.CalculationData(10000, 0, 5);
+            var rounding = new Afa.CalculationRoundings.FullValueRounding();
+            var calc = new Afa.CalculationMethods.Linear();
+
+            TestResult(rounding.Calculate(calc, data, 0), 10000, 0);
+            TestResult(rounding.Calculate(calc, data, 1), 8000, 2000);
+            TestResult(rounding.Calculate(calc, data, 2), 6000, 2000);
+            TestResult(rounding.Calculate(calc, data, 3), 4000, 2000);
+            TestResult(rounding.Calculate(calc, data, 4), 2000, 2000);
+            TestResult(rounding.Calculate(calc, data, 5), 0, 2000);
+        }
+    }
+}
